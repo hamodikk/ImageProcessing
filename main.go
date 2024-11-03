c95614cf0c54c3a60df316fdc281bac6fb619901
hamodikk <kkucukengin@gmail.com>
2024-11-03 17:11:36 -0600

    Added flag for concurrency/sequential switch

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	imageprocessing "goroutines_pipeline/image_processing"
 	"image"
 	"log"
 	"os"
+	"runtime"
 	"strings"
+	"time"
 )
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
 
 func checkFileExists(filepath string) error {
 	if _, err := os.Stat(filepath); os.IsNotExist(err) {
@@ -22,91 +29,158 @@ type Job struct {
 	OutPath   string
 }
 
-func loadImage(paths []string) <-chan Job {
+// Concurrent version of loadImage
+func loadImageConcurrent(paths []string) <-chan Job {
 	out := make(chan Job)
 	go func() {
 		defer close(out)
-
-		// For each input path create a job and add it to
-		// the out channel
 		for _, p := range paths {
 			if err := checkFileExists(p); err != nil {
 				log.Printf("Error loading image %s: %v\n", p, err)
 				continue
 			}
-
-			job := Job{InputPath: p,
-				Image:   imageprocessing.ReadImage(p),
-				OutPath: strings.Replace(p, "images/", "images/output/", 1),
+			job := Job{
+				InputPath: p,
+				Image:     imageprocessing.ReadImage(p),
+				OutPath:   strings.Replace(p, "images/", "images/output/", 1),
 			}
-
 			out <- job
 		}
 	}()
 	return out
 }
 
-func resize(input <-chan Job) <-chan Job {
+// Sequential version of loadImage
+func loadImageSequential(paths []string) []Job {
+	var jobs []Job
+	for _, p := range paths {
+		if err := checkFileExists(p); err != nil {
+			log.Printf("Error loading image %s: %v\n", p, err)
+			continue
+		}
+		job := Job{
+			InputPath: p,
+			Image:     imageprocessing.ReadImage(p),
+			OutPath:   strings.Replace(p, "images/", "images/output/", 1),
+		}
+		jobs = append(jobs, job)
+	}
+	return jobs
+}
+
+// Concurrent version of resize
+func resizeConcurrent(input <-chan Job) <-chan Job {
 	out := make(chan Job)
 	go func() {
-		// For each input job, create a new job after resize and add it to
-		// the out channel
-		for job := range input { // Read from the channel
+		defer close(out)
+		for job := range input {
 			job.Image = imageprocessing.Resize(job.Image)
 			out <- job
 		}
-		close(out)
 	}()
 	return out
 }
 
-func convertToGrayscale(input <-chan Job) <-chan Job {
+// Sequential version of resize
+func resizeSequential(jobs []Job) []Job {
+	for i := range jobs {
+		jobs[i].Image = imageprocessing.Resize(jobs[i].Image)
+	}
+	return jobs
+}
+
+// Concurrent version of convertToGrayscale
+func convertToGrayscaleConcurrent(input <-chan Job) <-chan Job {
 	out := make(chan Job)
 	go func() {
-		for job := range input { // Read from the channel
+		defer close(out)
+		for job := range input {
 			job.Image = imageprocessing.Grayscale(job.Image)
 			out <- job
 		}
-		close(out)
 	}()
 	return out
 }
 
-func saveImage(input <-chan Job) <-chan bool {
+// Sequential version of convertToGrayscale
+func convertToGrayscaleSequential(jobs []Job) []Job {
+	for i := range jobs {
+		jobs[i].Image = imageprocessing.Grayscale(jobs[i].Image)
+	}
+	return jobs
+}
+
+// Concurrent version of saveImage
+func saveImageConcurrent(input <-chan Job) <-chan bool {
 	out := make(chan bool)
 	go func() {
 		defer close(out)
-		for job := range input { // Read from the channel
+		for job := range input {
 			err := imageprocessing.WriteImage(job.OutPath, job.Image)
-			if err != nil {
-				log.Printf("Error saving image to %s: %v\n", job.OutPath, err)
-				out <- false
-			} else {
-				out <- true
-			}
+			out <- err == nil
 		}
 	}()
 	return out
 }
 
-func main() {
+// Sequential version of saveImage
+func saveImageSequential(jobs []Job) []bool {
+	var results []bool
+	for _, job := range jobs {
+		err := imageprocessing.WriteImage(job.OutPath, job.Image)
+		results = append(results, err == nil)
+	}
+	return results
+}
 
-	imagePaths := []string{"images/image1.jpeg",
+func main() {
+	useGoroutines := flag.Bool("goroutines", false, "Run the pipeline with goroutines")
+	flag.Parse()
+
+	imagePaths := []string{
+		"images/image1.jpeg",
 		"images/image2.jpeg",
 		"images/image3.jpeg",
 		"images/image4.jpeg",
 	}
 
-	channel1 := loadImage(imagePaths)
-	channel2 := resize(channel1)
-	channel3 := convertToGrayscale(channel2)
-	writeResults := saveImage(channel3)
+	start := time.Now()
 
-	for success := range writeResults {
-		if success {
-			fmt.Println("Success!")
-		} else {
-			fmt.Println("Failed!")
+	if *useGoroutines {
+		fmt.Println("Running with goroutines...")
+
+		// Run each stage with goroutines
+		channel1 := loadImageConcurrent(imagePaths)
+		channel2 := resizeConcurrent(channel1)
+		channel3 := convertToGrayscaleConcurrent(channel2)
+		writeResults := saveImageConcurrent(channel3)
+
+		for success := range writeResults {
+			if success {
+				fmt.Println("Success!")
+			} else {
+				fmt.Println("Failed!")
+			}
+		}
+
+	} else {
+		fmt.Println("Running sequentially...")
+
+		// Run each stage sequentially
+		jobs := loadImageSequential(imagePaths)
+		jobs = resizeSequential(jobs)
+		jobs = convertToGrayscaleSequential(jobs)
+		results := saveImageSequential(jobs)
+
+		for _, success := range results {
+			if success {
+				fmt.Println("Success!")
+			} else {
+				fmt.Println("Failed!")
+			}
 		}
 	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Processing took %s\n", elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -5,40 +5,57 @@ import (
 )
 
 func TestCheckFileExists(t *testing.T) {
-	err := checkFileExists("images/image1.jpg")
+	err := checkFileExists("images/image1.jpeg")
 	if err != nil {
 		t.Fatalf("expected file to exist, got error: %v", err)
 	}
 
-	err = checkFileExists("images/image5.jpg")
+	err = checkFileExists("images/nonexistent_image.jpg")
 	if err == nil {
 		t.Fatalf("expected error for nonexistent file, got nil")
 	}
 }
 
 func TestLoadImage(t *testing.T) {
-	imagePaths := []string{"images/image1.jpg"}
+	imagePaths := []string{"images/image1.jpeg"}
 
-	jobChannel := loadImage(imagePaths)
+	jobChannel := loadImageConcurrent(imagePaths) // Testing concurrent version
 
-	job := <-jobChannel
-	if job.Image == nil {
-		t.Fatalf("expected image, got nil")
+	job, ok := <-jobChannel
+	if !ok || job.Image == nil {
+		t.Fatalf("expected image, got nil or closed channel")
 	}
 }
 
-func BenchmarkImageProcessing(b *testing.B) {
-	imagePaths := []string{"images/image1.jpg"}
+func BenchmarkImageProcessingConcurrent(b *testing.B) {
+	imagePaths := []string{"images/image1.jpeg", "images/image2.jpeg", "images/image3.jpeg", "images/image4.jpeg"}
 
 	for i := 0; i < b.N; i++ {
-		channel1 := loadImage(imagePaths)
-		channel2 := resize(channel1)
-		channel3 := convertToGrayscale(channel2)
-		writeResults := saveImage(channel3)
+		channel1 := loadImageConcurrent(imagePaths)
+		channel2 := resizeConcurrent(channel1)
+		channel3 := convertToGrayscaleConcurrent(channel2)
+		writeResults := saveImageConcurrent(channel3)
 
 		for success := range writeResults {
 			if !success {
-				b.Fatalf("failed to save image")
+				b.Fatalf("failed to save image in concurrent processing")
+			}
+		}
+	}
+}
+
+func BenchmarkImageProcessingSequential(b *testing.B) {
+	imagePaths := []string{"images/image1.jpeg", "images/image2.jpeg", "images/image3.jpeg", "images/image4.jpeg"}
+
+	for i := 0; i < b.N; i++ {
+		jobs := loadImageSequential(imagePaths)
+		jobs = resizeSequential(jobs)
+		jobs = convertToGrayscaleSequential(jobs)
+		results := saveImageSequential(jobs)
+
+		for _, success := range results {
+			if !success {
+				b.Fatalf("failed to save image in sequential processing")
 			}
 		}
 	}
